986c989d458b7464d76d9bdc69ff4e4351e622ae
iamjenjensha <sjenjensha@gmail.com>
2017-11-12 10:18:54 +0100

    Add routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-
-
 $route['default_controller'] = '';
+
+// Admin section
+$route['admin/login']  = 'admin_c/login';
+$route['admin/forgot_password']= 'admin_c/forgot_password';
+$route['admin/ngos']= 'admin_c/ngos';
